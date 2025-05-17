69659d2c97d214cb50056153ee25f59facc0e6a9
BarDab <barpl7@gmail.com>
2025-05-17 20:09:27 +0200

    VECM until - calculating and interpreting Impulse Response Function for the reparametrized model (included)

--- utils.py
+++ utils.py
@@ -1,53 +1,91 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-import statsmodels.stats.diagnostic as smd 
+import statsmodels.stats.diagnostic as smd
+from statsmodels.tsa.api import VAR
+
 
 def adf_test(series, max_aug=10, version='n'):
-    
     results = []
 
     y = series.diff()
     X = pd.DataFrame({'y_lag': series.shift()})
 
-    if version == 'c' or version == 't': # constant to be added optionally 
+    if version == 'c' or version == 't':  # constant to be added optionally
         X = sm.add_constant(X)
-    if version == 't': # (deterministic) trend component to be added optionally
+    if version == 't':  # (deterministic) trend component to be added optionally
         X['trend'] = range(len(X))
 
-    for i in range(0, max_aug): # iterating through different numbers of augmentations
-        
-        for aug in range(1, i+1): # adding augmentations one by one until its current amount is reached
-            X['aug_'+str(aug)] = y.shift(aug)
+    for i in range(0, max_aug):  # iterating through different numbers of augmentations
 
-        model = sm.OLS(series.diff(), X, missing='drop').fit() # fitting a linear regression with OLS
+        for aug in range(1, i + 1):  # adding augmentations one by one until its current amount is reached
+            X['aug_' + str(aug)] = y.shift(aug)
 
-        ts = model.tvalues['y_lag'] # test statistic
-        nobs = model.nobs # number of observations
+        model = sm.OLS(series.diff(), X, missing='drop').fit()  # fitting a linear regression with OLS
 
-        if version == 'n': # critical values for basic version of ADF
+        ts = model.tvalues['y_lag']  # test statistic
+        nobs = model.nobs  # number of observations
+
+        if version == 'n':  # critical values for basic version of ADF
             if nobs > 500:
-                cv1 = -2.567; cv5 = -1.941; cv10 = -1.616 # critical values for more than 500 observations
+                cv1 = -2.567;
+                cv5 = -1.941;
+                cv10 = -1.616  # critical values for more than 500 observations
             else:
-                cv1 = np.nan; cv5 = np.nan; cv10 = np.nan # if number of observations is lower than 500, we should check the critical values manually
-        if version == 'c': # critical values for version with constant
+                cv1 = np.nan;
+                cv5 = np.nan;
+                cv10 = np.nan  # if number of observations is lower than 500, we should check the critical values manually
+        if version == 'c':  # critical values for version with constant
             if nobs > 500:
-                cv1 = -3.434; cv5 = -2.863; cv10 = -2.568 # critical values for more than 500 observations
+                cv1 = -3.434;
+                cv5 = -2.863;
+                cv10 = -2.568  # critical values for more than 500 observations
             else:
-                cv1 = np.nan; cv5 = np.nan; cv10 = np.nan # if number of observations is lower than 500, we should check the critical values manually
-        if version == 't': # critical values for version with constant and (deterministic) trend component
+                cv1 = np.nan;
+                cv5 = np.nan;
+                cv10 = np.nan  # if number of observations is lower than 500, we should check the critical values manually
+        if version == 't':  # critical values for version with constant and (deterministic) trend component
             if nobs > 500:
-                cv1 = -3.963; cv5 = -3.413; cv10 = -3.128 # critical values for more than 500 observations
+                cv1 = -3.963;
+                cv5 = -3.413;
+                cv10 = -3.128  # critical values for more than 500 observations
             else:
-                cv1 = np.nan; cv5 = np.nan; cv10 = np.nan # if number of observations is lower than 500, we should check the critical values manually
+                cv1 = np.nan;
+                cv5 = np.nan;
+                cv10 = np.nan  # if number of observations is lower than 500, we should check the critical values manually
 
-        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=5); bg_pvalue5 = round(bg_test5[1],4)
-        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=10); bg_pvalue10 = round(bg_test5[1],4)
-        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=15); bg_pvalue15 = round(bg_test5[1],4)
+        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=5);
+        bg_pvalue5 = round(bg_test5[1], 4)
+        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=10);
+        bg_pvalue10 = round(bg_test5[1], 4)
+        bg_test5 = smd.acorr_breusch_godfrey(model, nlags=15);
+        bg_pvalue15 = round(bg_test5[1], 4)
 
         results.append([i, ts, cv1, cv5, cv10, bg_pvalue5, bg_pvalue10, bg_pvalue15])
 
     results_df = pd.DataFrame(results)
-    results_df.columns = ['number of augmentations', 'ADF test statistic', 'ADF critival value (1%)', 'ADF critival value (5%)', 'ADF critival value (10%)', 'BG test (5 lags) (p-value)', 'BG test (10 lags) (p-value)', 'BG test (15 lags) (p-value)']
-    
+    results_df.columns = ['number of augmentations', 'ADF test statistic', 'ADF critival value (1%)',
+                          'ADF critival value (5%)', 'ADF critival value (10%)', 'BG test (5 lags) (p-value)',
+                          'BG test (10 lags) (p-value)', 'BG test (15 lags) (p-value)']
+
     return results_df
+
+
+def selct_var_order(data, max_lags=12):
+    # Create a VAR model
+    model = VAR(data, freq='D')
+
+    # Determine information criteria for different lag orders
+    results = {}
+    for i in range(1, max_lags + 1):
+        results[i] = model.select_order(i)
+
+    # Create a DataFrame to display the results
+    criteria = ['aic', 'bic', 'hqic', 'fpe']
+    order_df = pd.DataFrame(index=range(1, max_lags + 1), columns=criteria)
+
+    for i in range(1, max_lags + 1):
+        for criterion in criteria:
+            order_df.loc[i, criterion] = getattr(results[i], criterion)
+
+    return order_df
